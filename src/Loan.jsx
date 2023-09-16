7529baa940565c74beba64dcd8c8ab0cdf1b33c0
Ananth Lakshman Kumar <ananthhockage@gmail.com>
2023-09-16 14:38:33 -0400

    Changes to be committed: 	modified:   src/App.jsx 	modified:   src/Loan.jsx

--- src/Loan.jsx
+++ src/Loan.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-const [loanDetails,setLoanDetails]=React.useState({
-    loanamount:"0",
-    loanterm:"0",
-    interest:"0"
+import {useState} from 'react'
 
-})
-function handleChange(event){
-    const {name,value}=event.target
-    setLoanDetails(prevData =>(
-        {
-            ...prevData,
-            [name]: value
-        }
-    ))
-
-
-
-}
 export default function Loan(){
+    const [loanDetails,setLoanDetails]=useState({
+        loanamount:0,
+        loanterm:0,
+        interest:0,
+        paid:0
+    
+    })
+    function handleChange(event){
+        const {name,value}=event.target
+
+        setLoanDetails(prevData =>(
+            {
+                ...prevData,
+                [name]: value
+            }
+        ))
+    
+    
+    
+    }
+    function handleSubmit(event){
+        event.preventDefault()
+        const tobepaid=(loanDetails.loanamount*loanDetails.loanterm*loanDetails.interest)/1200
+        setLoanDetails(prevData=>({
+            ...prevData,
+            paid:tobepaid
+        }))
+        
+    }
     return (
+     <form onSubmit={handleSubmit}>    
         <div className='Loan'>
             <label htmlFor='loanamount'>Loan Amount</label>
+            <br/>
             <input 
             type='text'
             name='loanamount'
@@ -29,7 +43,33 @@ export default function Loan(){
             onChange={handleChange}
             id='loanamount'
             ></input>
-            
+            <br/>
+            <br/>
+            <label htmlFor='loanterm'>Loan Term</label>
+            <br/>
+            <input 
+            type='text'
+            name='loanterm'
+            value={loanDetails.loanterm}
+            placeholder='Enter loan Term'
+            onChange={handleChange}
+            id='loanterm'
+            ></input>
+            <br/>
+            <br/>
+            <label htmlFor='interest'>Interest rate </label>
+            <br/>
+            <input 
+            type='text'
+            name='interest'
+            value={loanDetails.interest}
+            placeholder='Enter loan Term'
+            onChange={handleChange}
+            id='interest'
+            ></input>
+            <h3>Interest paid monthly : {loanDetails.paid}</h3>
         </div>
+        <button type='submit'>Calculate</button>
+    </form>
     )
 }
